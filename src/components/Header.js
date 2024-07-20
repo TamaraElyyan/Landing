b5ed082023a9761f3d3a0cdb0dbbb06a69998adf
TamaraElyyan <tamaraelyyan1@gmail.com>
2024-07-20 04:55:54 +0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import './../CSS/Header.css';
+
+
+const Header = () => {
+  return (
+    <header className="header d-flex justify-content-between align-items-center p-5 bg-white">
+      <nav className="navbar navbar-expand-sm navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a href="#" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="h3 h3-title">Landing</div>
+      <button className="buy-now-btn ">Buy Now</button>
+    </header>
+  );
+};
+
+export default Header;
