ae22398611dc014c8fb9a3e483769657148b7aea
TamaraElyyan <tamaraelyyan1@gmail.com>
2024-07-24 22:48:32 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import HeroSection from './components/Hero/HeroSection';
+import FeaturesSection from './components/Feature/FeaturesSection';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
+}
+
+export default App;
